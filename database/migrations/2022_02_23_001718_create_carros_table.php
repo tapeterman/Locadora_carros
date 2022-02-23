e576bb387f3eac930e270d3df0249c3b3bff0e4c
tapeterman <tapetermann@outlook.com.br>
2022-02-22 21:43:51 -0300

    configurando BD e estruturando as migrations

--- database/migrations/2022_02_23_001718_create_carros_table.php
+++ database/migrations/2022_02_23_001718_create_carros_table.php
@@ -15,7 +15,14 @@ class CreateCarrosTable extends Migration
     {
         Schema::create('carros', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->string('placa', 10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
             $table->timestamps();
+    
+            //foreign key (constraints)
+            $table->foreign('modelo_id')->references('id')->on('modelos');
         });
     }
 
@@ -26,6 +33,11 @@ class CreateCarrosTable extends Migration
      */
     public function down()
     {
+        Schema::table('carros',function(Blueprint $table){
+            $table->DropForeign('carros_modelos_id_foreign');
+            $table->dropColum('modelos_id');
+        });
+
         Schema::dropIfExists('carros');
     }
 }
---
+++ database/migrations/2022_02_23_001823_create_locacoes_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocacoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locacoes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('carro_id');
+            $table->dateTime('data_inicio_periodo');
+            $table->dateTime('data_final_previsto_periodo');
+            $table->dateTime('data_final_realizado_periodo');
+            $table->float('valor_diaria', 8,2);
+            $table->integer('km_inicial');
+            $table->integer('km_final');
+            $table->timestamps();
+    
+            //foreign key (constraints)
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->foreign('carro_id')->references('id')->on('carros');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('locacoes',function(Blueprint $table){
+            $table->DropForeign('locacoes_cliente_id_foreign');
+            $table->DropForeign('locacoes_carro_id_foreign');
+            $table->dropColum('cliente_id');
+            $table->dropColum('carro_id');
+        });
+
+        Schema::dropIfExists('locacaos');
+    }
+}
